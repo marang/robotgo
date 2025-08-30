5816218a1e5d108d854f12a084517c8e444f77fe
marang <1550038+marang@users.noreply.github.com>
2025-08-30 23:32:02 +0200

    fix: close wayland block correctly

--- key/keypress.h
+++ key/keypress.h
@@ -20,16 +20,16 @@
 #elif defined(IS_LINUX)
 #if defined(DISPLAY_SERVER_WAYLAND)
         #include <xkbcommon/xkbcommon.h>
-        enum _MMKeyFlags {
+        typedef xkb_mod_mask_t MMKeyFlags;
+        enum MMKeyFlagsBits {
                 MOD_NONE = 0,
                 MOD_META = XKB_MOD_MASK_LOGO,
                 MOD_ALT = XKB_MOD_MASK_ALT,
                 MOD_CONTROL = XKB_MOD_MASK_CTRL,
                 MOD_SHIFT = XKB_MOD_MASK_SHIFT
         };
-        typedef xkb_mod_mask_t MMKeyFlags;
 #else
-        enum _MMKeyFlags {
+        enum MMKeyFlagsBits {
                 MOD_NONE = 0,
                 MOD_META = Mod4Mask,
                 MOD_ALT = Mod1Mask,
