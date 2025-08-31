2cd97194570a9ff459c89f56ddea8a911fbfd1a6
marang <1550038+marang@users.noreply.github.com>
2025-08-31 11:39:05 +0200

    feat: use wlr-screencopy for wayland capture

--- screen/screengrab_c.h
+++ screen/screengrab_c.h
@@ -2,199 +2,227 @@
 #include <stdlib.h> /* malloc() */
 
 #if defined(IS_MACOSX)
-	#include <OpenGL/OpenGL.h>
-	#include <OpenGL/gl.h>
-	#include <ApplicationServices/ApplicationServices.h>
-	#include <ScreenCaptureKit/ScreenCaptureKit.h>
+#include <ApplicationServices/ApplicationServices.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl.h>
+#include <ScreenCaptureKit/ScreenCaptureKit.h>
 #elif defined(IS_LINUX)
-        #include <X11/Xlib.h>
-        #include <X11/Xutil.h>
-        #include "../base/xdisplay_c.h"
-        MMBitmapRef capture_screen_wayland(int32_t x, int32_t y, int32_t w, int32_t h, int32_t display_id, int8_t isPid);
+#include "../base/xdisplay_c.h"
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+MMBitmapRef capture_screen_wayland(int32_t x, int32_t y, int32_t w, int32_t h,
+                                   int32_t display_id, int8_t isPid);
 #elif defined(IS_WINDOWS)
-        #include <string.h>
+#include <string.h>
 #endif
 #include "screen_c.h"
 
 #if defined(IS_MACOSX) && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ > 140400
-	static CGImageRef capture15(CGDirectDisplayID id, CGRect diIntersectDisplayLocal, CGColorSpaceRef colorSpace) {
-		dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-		__block CGImageRef image1 = nil;
-		[SCShareableContent getShareableContentWithCompletionHandler:^(SCShareableContent* content, NSError* error) {
-			@autoreleasepool {
-				if (error) {
-					dispatch_semaphore_signal(semaphore);
-					return;
-				}
-				
-				SCDisplay* target = nil;
-				for (SCDisplay *display in content.displays) {
-					if (display.displayID == id) {
-						target = display;
-						break;
-					}
-				}
-				if (!target) {
-					dispatch_semaphore_signal(semaphore);
-					return;
-				}
+static CGImageRef capture15(CGDirectDisplayID id,
+                            CGRect diIntersectDisplayLocal,
+                            CGColorSpaceRef colorSpace) {
+  dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+  __block CGImageRef image1 = nil;
+  [SCShareableContent getShareableContentWithCompletionHandler:^(
+                          SCShareableContent *content, NSError *error) {
+    @autoreleasepool {
+      if (error) {
+        dispatch_semaphore_signal(semaphore);
+        return;
+      }
 
-				SCContentFilter* filter = [[SCContentFilter alloc] initWithDisplay:target excludingWindows:@[]];
-				SCStreamConfiguration* config = [[SCStreamConfiguration alloc] init];
-				config.queueDepth = 5;
-				config.sourceRect = diIntersectDisplayLocal;
-				config.width = diIntersectDisplayLocal.size.width * sys_scale(id);
-				config.height = diIntersectDisplayLocal.size.height * sys_scale(id);
-				config.scalesToFit = false;
-				config.captureResolution = 1;
+      SCDisplay *target = nil;
+      for (SCDisplay *display in content.displays) {
+        if (display.displayID == id) {
+          target = display;
+          break;
+        }
+      }
+      if (!target) {
+        dispatch_semaphore_signal(semaphore);
+        return;
+      }
 
-				[SCScreenshotManager captureImageWithFilter:filter
-					configuration:config
-					completionHandler:^(CGImageRef img, NSError* error) {
-						if (!error) {
-							image1 = CGImageCreateCopyWithColorSpace(img, colorSpace);
-						}
-						dispatch_semaphore_signal(semaphore);
-				}];
-			}
-		}];
+      SCContentFilter *filter =
+          [[SCContentFilter alloc] initWithDisplay:target excludingWindows:@[]];
+      SCStreamConfiguration *config = [[SCStreamConfiguration alloc] init];
+      config.queueDepth = 5;
+      config.sourceRect = diIntersectDisplayLocal;
+      config.width = diIntersectDisplayLocal.size.width * sys_scale(id);
+      config.height = diIntersectDisplayLocal.size.height * sys_scale(id);
+      config.scalesToFit = false;
+      config.captureResolution = 1;
 
-		dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-		dispatch_release(semaphore);
-		return image1;
-	}
+      [SCScreenshotManager
+          captureImageWithFilter:filter
+                   configuration:config
+               completionHandler:^(CGImageRef img, NSError *error) {
+                 if (!error) {
+                   image1 = CGImageCreateCopyWithColorSpace(img, colorSpace);
+                 }
+                 dispatch_semaphore_signal(semaphore);
+               }];
+    }
+  }];
+
+  dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+  dispatch_release(semaphore);
+  return image1;
+}
 #endif
 
-MMBitmapRef copyMMBitmapFromDisplayInRect(MMRectInt32 rect, int32_t display_id, int8_t isPid) {
+MMBitmapRef copyMMBitmapFromDisplayInRect(MMRectInt32 rect, int32_t display_id,
+                                          int8_t isPid) {
 #if defined(IS_MACOSX)
-	MMBitmapRef bitmap = NULL;
-	uint8_t *buffer = NULL;
-	size_t bufferSize = 0;
+  MMBitmapRef bitmap = NULL;
+  uint8_t *buffer = NULL;
+  size_t bufferSize = 0;
 
-	CGDirectDisplayID displayID = (CGDirectDisplayID) display_id;
-	if (displayID == -1 || displayID == 0) {
-		displayID = CGMainDisplayID();
-	}
+  CGDirectDisplayID displayID = (CGDirectDisplayID)display_id;
+  if (displayID == -1 || displayID == 0) {
+    displayID = CGMainDisplayID();
+  }
 
-	MMPointInt32 o = rect.origin; MMSizeInt32 s = rect.size;
-	#if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ > 140400
-		CGColorSpaceRef color = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
-		CGImageRef image = capture15(displayID, CGRectMake(o.x, o.y, s.w, s.h), color);
-		CGColorSpaceRelease(color);
-	#else
-		// This API is deprecated in macos 15, use ScreenCaptureKit's captureScreenshot
-		CGImageRef image = CGDisplayCreateImageForRect(displayID, CGRectMake(o.x, o.y, s.w, s.h));
-	#endif
-	if (!image) { return NULL; }
-	
-	CFDataRef imageData = CGDataProviderCopyData(CGImageGetDataProvider(image));
-	if (!imageData) { return NULL; }
-
-	bufferSize = CFDataGetLength(imageData);
-	buffer = malloc(bufferSize);
-	CFDataGetBytes(imageData, CFRangeMake(0, bufferSize), buffer);
-
-	bitmap = createMMBitmap_c(buffer, 
-			CGImageGetWidth(image), CGImageGetHeight(image), CGImageGetBytesPerRow(image), 
-			CGImageGetBitsPerPixel(image), CGImageGetBitsPerPixel(image) / 8);
-
-	CFRelease(imageData);
-	CGImageRelease(image);
-
-	return bitmap;
-#elif defined(IS_LINUX)
-	MMBitmapRef bitmap;
-	Display *display;
-	if (display_id == -1) {
-		display = XOpenDisplay(NULL);
-	} else {
-		display = XGetMainDisplay();
-	}
-
-       MMPointInt32 o = rect.origin; MMSizeInt32 s = rect.size;
-       unsigned long mask = (1UL << DefaultDepth(display, DefaultScreen(display))) - 1;
-       XImage *image = XGetImage(display, XDefaultRootWindow(display),
-                                                       (int)o.x, (int)o.y, (unsigned int)s.w, (unsigned int)s.h,
-                                                       mask, ZPixmap);
-       if (image == NULL) {
-               XCloseDisplay(display);
-               return NULL;
-       }
-       XCloseDisplay(display);
-
-	bitmap = createMMBitmap_c((uint8_t *)image->data, 
-				s.w, s.h, (size_t)image->bytes_per_line, 
-				(uint8_t)image->bits_per_pixel, (uint8_t)image->bits_per_pixel / 8);
-	image->data = NULL; /* Steal ownership of bitmap data so we don't have to copy it. */
-	XDestroyImage(image);
-
-	return bitmap;
-#elif defined(IS_WINDOWS)
-	MMBitmapRef bitmap;
-	void *data;
-	HDC screen = NULL, screenMem = NULL;
-	HBITMAP dib;
-	BITMAPINFO bi;
-
-	int32_t x = rect.origin.x, y = rect.origin.y;
-	int32_t w = rect.size.w, h = rect.size.h;
-
-	/* Initialize bitmap info. */
-	bi.bmiHeader.biSize = sizeof(bi.bmiHeader);
-   	bi.bmiHeader.biWidth = (long) w;
-   	bi.bmiHeader.biHeight = -(long) h; /* Non-cartesian, please */
-   	bi.bmiHeader.biPlanes = 1;
-   	bi.bmiHeader.biBitCount = 32;
-   	bi.bmiHeader.biCompression = BI_RGB;
-   	bi.bmiHeader.biSizeImage = (DWORD)(4 * w * h);
-	bi.bmiHeader.biXPelsPerMeter = 0;
-	bi.bmiHeader.biYPelsPerMeter = 0;
-	bi.bmiHeader.biClrUsed = 0;
-	bi.bmiHeader.biClrImportant = 0;
-
-	HWND hwnd;
-	if (display_id == -1 || isPid == 0) {
-	// 	screen = GetDC(NULL); /* Get entire screen */
-		hwnd = GetDesktopWindow();
-	} else {
-		hwnd = (HWND) (uintptr) display_id;
-	}
-	screen = GetDC(hwnd);
-	
-	if (screen == NULL) { return NULL; }
-
-	// Todo: Use DXGI
-	screenMem = CreateCompatibleDC(screen);
-	/* Get screen data in display device context. */
-   	dib = CreateDIBSection(screen, &bi, DIB_RGB_COLORS, &data, NULL, 0);
-
-	/* Copy the data into a bitmap struct. */
-	BOOL b = (screenMem == NULL) || 
-		SelectObject(screenMem, dib) == NULL ||
-	    !BitBlt(screenMem, (int)0, (int)0, (int)w, (int)h, screen, x, y, SRCCOPY);
-	if (b) {
-		/* Error copying data. */
-		ReleaseDC(hwnd, screen);
-		DeleteObject(dib);
-		if (screenMem != NULL) { DeleteDC(screenMem); }
-
-		return NULL;
-	}
-
-	bitmap = createMMBitmap_c(NULL, w, h, 4 * w, (uint8_t)bi.bmiHeader.biBitCount, 4);
-
-	/* Copy the data to our pixel buffer. */
-	if (bitmap != NULL) {
-		bitmap->imageBuffer = malloc(bitmap->bytewidth * bitmap->height);
-		memcpy(bitmap->imageBuffer, data, bitmap->bytewidth * bitmap->height);
-	}
-
-	ReleaseDC(hwnd, screen);
-	DeleteObject(dib);
-	DeleteDC(screenMem);
-
-	return bitmap;
+  MMPointInt32 o = rect.origin;
+  MMSizeInt32 s = rect.size;
+#if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ > 140400
+  CGColorSpaceRef color = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
+  CGImageRef image =
+      capture15(displayID, CGRectMake(o.x, o.y, s.w, s.h), color);
+  CGColorSpaceRelease(color);
+#else
+  // This API is deprecated in macos 15, use ScreenCaptureKit's
+  // captureScreenshot
+  CGImageRef image =
+      CGDisplayCreateImageForRect(displayID, CGRectMake(o.x, o.y, s.w, s.h));
 #endif
+  if (!image) {
+    return NULL;
+  }
+
+  CFDataRef imageData = CGDataProviderCopyData(CGImageGetDataProvider(image));
+  if (!imageData) {
+    return NULL;
+  }
+
+  bufferSize = CFDataGetLength(imageData);
+  buffer = malloc(bufferSize);
+  CFDataGetBytes(imageData, CFRangeMake(0, bufferSize), buffer);
+
+  bitmap = createMMBitmap_c(
+      buffer, CGImageGetWidth(image), CGImageGetHeight(image),
+      CGImageGetBytesPerRow(image), CGImageGetBitsPerPixel(image),
+      CGImageGetBitsPerPixel(image) / 8);
+
+  CFRelease(imageData);
+  CGImageRelease(image);
+
+  return bitmap;
+#elif defined(IS_LINUX)
+  MMBitmapRef bitmap;
+  Display *display;
+  if (display_id == -1) {
+    display = XOpenDisplay(NULL);
+  } else {
+    display = XGetMainDisplay();
+  }
+
+  MMPointInt32 o = rect.origin;
+  MMSizeInt32 s = rect.size;
+  unsigned long mask =
+      (1UL << DefaultDepth(display, DefaultScreen(display))) - 1;
+  XImage *image =
+      XGetImage(display, XDefaultRootWindow(display), (int)o.x, (int)o.y,
+                (unsigned int)s.w, (unsigned int)s.h, mask, ZPixmap);
+  if (image == NULL) {
+    XCloseDisplay(display);
+    return NULL;
+  }
+  XCloseDisplay(display);
+
+  bitmap = createMMBitmap_c(
+      (uint8_t *)image->data, s.w, s.h, (size_t)image->bytes_per_line,
+      (uint8_t)image->bits_per_pixel, (uint8_t)image->bits_per_pixel / 8);
+  image->data =
+      NULL; /* Steal ownership of bitmap data so we don't have to copy it. */
+  XDestroyImage(image);
+
+  return bitmap;
+#elif defined(IS_WINDOWS)
+  MMBitmapRef bitmap;
+  void *data;
+  HDC screen = NULL, screenMem = NULL;
+  HBITMAP dib;
+  BITMAPINFO bi;
+
+  int32_t x = rect.origin.x, y = rect.origin.y;
+  int32_t w = rect.size.w, h = rect.size.h;
+
+  /* Initialize bitmap info. */
+  bi.bmiHeader.biSize = sizeof(bi.bmiHeader);
+  bi.bmiHeader.biWidth = (long)w;
+  bi.bmiHeader.biHeight = -(long)h; /* Non-cartesian, please */
+  bi.bmiHeader.biPlanes = 1;
+  bi.bmiHeader.biBitCount = 32;
+  bi.bmiHeader.biCompression = BI_RGB;
+  bi.bmiHeader.biSizeImage = (DWORD)(4 * w * h);
+  bi.bmiHeader.biXPelsPerMeter = 0;
+  bi.bmiHeader.biYPelsPerMeter = 0;
+  bi.bmiHeader.biClrUsed = 0;
+  bi.bmiHeader.biClrImportant = 0;
+
+  HWND hwnd;
+  if (display_id == -1 || isPid == 0) {
+    // 	screen = GetDC(NULL); /* Get entire screen */
+    hwnd = GetDesktopWindow();
+  } else {
+    hwnd = (HWND)(uintptr)display_id;
+  }
+  screen = GetDC(hwnd);
+
+  if (screen == NULL) {
+    return NULL;
+  }
+
+  // Todo: Use DXGI
+  screenMem = CreateCompatibleDC(screen);
+  /* Get screen data in display device context. */
+  dib = CreateDIBSection(screen, &bi, DIB_RGB_COLORS, &data, NULL, 0);
+
+  /* Copy the data into a bitmap struct. */
+  BOOL b =
+      (screenMem == NULL) || SelectObject(screenMem, dib) == NULL ||
+      !BitBlt(screenMem, (int)0, (int)0, (int)w, (int)h, screen, x, y, SRCCOPY);
+  if (b) {
+    /* Error copying data. */
+    ReleaseDC(hwnd, screen);
+    DeleteObject(dib);
+    if (screenMem != NULL) {
+      DeleteDC(screenMem);
+    }
+
+    return NULL;
+  }
+
+  bitmap =
+      createMMBitmap_c(NULL, w, h, 4 * w, (uint8_t)bi.bmiHeader.biBitCount, 4);
+
+  /* Copy the data to our pixel buffer. */
+  if (bitmap != NULL) {
+    bitmap->imageBuffer = malloc(bitmap->bytewidth * bitmap->height);
+    memcpy(bitmap->imageBuffer, data, bitmap->bytewidth * bitmap->height);
+  }
+
+  ReleaseDC(hwnd, screen);
+  DeleteObject(dib);
+  DeleteDC(screenMem);
+
+  return bitmap;
+#endif
+}
+
+MMRGBHex mmrgb_hex_at(MMBitmapRef bitmap, int32_t x, int32_t y) {
+  return MMRGBHexAtPoint(bitmap, x, y);
 }
 
 #if defined(IS_LINUX)
--- screen/screengrab_wayland.c
+++ screen/screengrab_wayland.c
@@ -1,15 +1,243 @@
 //go:build ignore
 // +build ignore
 
-// This file is included from screengrab_c.h, which already provides the
-// necessary declarations and includes. Avoid including headers here to prevent
-// duplicate symbol definitions when screengrab_wayland.c is embedded.
+// This file is included from screengrab_c.h, which provides necessary declarations.
+// It implements Wayland screen capture via the wlr-screencopy protocol without
+// relying on external utilities.
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <wayland-client.h>
+#include "../wlr-screencopy-unstable-v1-client-protocol.h"
 
 #if defined(IS_LINUX)
-// capture_screen_wayland attempts to capture the screen using a Wayland protocol.
-// This is a stub that currently returns NULL to signal unsupported capture.
-MMBitmapRef capture_screen_wayland(int32_t x, int32_t y, int32_t w, int32_t h, int32_t display_id, int8_t isPid) {
-        (void)x; (void)y; (void)w; (void)h; (void)display_id; (void)isPid;
-        return NULL;
+
+struct output {
+  struct wl_list link;
+  struct wl_output *wl_output;
+};
+
+struct capture {
+  struct wl_display *display;
+  struct wl_registry *registry;
+  struct wl_shm *shm;
+  struct zwlr_screencopy_manager_v1 *manager;
+  struct zwlr_screencopy_frame_v1 *frame;
+  struct wl_buffer *buffer;
+  struct wl_list outputs;
+  void *data;
+  int width;
+  int height;
+  int stride;
+  int done;
+  int failed;
+};
+
+static void registry_global(void *data, struct wl_registry *registry, uint32_t name,
+                            const char *interface, uint32_t version) {
+  struct capture *cap = data;
+  if (strcmp(interface, wl_shm_interface.name) == 0) {
+    cap->shm = wl_registry_bind(registry, name, &wl_shm_interface, 1);
+  } else if (strcmp(interface, zwlr_screencopy_manager_v1_interface.name) == 0) {
+    cap->manager = wl_registry_bind(registry, name, &zwlr_screencopy_manager_v1_interface, 3);
+  } else if (strcmp(interface, wl_output_interface.name) == 0) {
+    struct output *out = malloc(sizeof(*out));
+    if (!out) {
+      return;
+    }
+    out->wl_output = wl_registry_bind(registry, name, &wl_output_interface, 2);
+    wl_list_insert(&cap->outputs, &out->link);
+  }
 }
-#endif
+
+static void registry_remove(void *data, struct wl_registry *registry, uint32_t name) {
+  (void)data;
+  (void)registry;
+  (void)name;
+}
+
+static const struct wl_registry_listener registry_listener = {
+    .global = registry_global,
+    .global_remove = registry_remove,
+};
+
+static void frame_buffer(void *data, struct zwlr_screencopy_frame_v1 *frame,
+                         uint32_t format, uint32_t width, uint32_t height,
+                         uint32_t stride) {
+  (void)frame;
+  (void)format;
+  struct capture *cap = data;
+  cap->width = (int)width;
+  cap->height = (int)height;
+  cap->stride = (int)stride;
+
+  char shm_name[64];
+  snprintf(shm_name, sizeof(shm_name), "/robotgo-wl-%d", getpid());
+  int fd = shm_open(shm_name, O_CREAT | O_EXCL | O_RDWR, 0600);
+  if (fd < 0) {
+    cap->failed = 1;
+    return;
+  }
+  shm_unlink(shm_name);
+  size_t size = (size_t)stride * height;
+  if (ftruncate(fd, (off_t)size) < 0) {
+    close(fd);
+    cap->failed = 1;
+    return;
+  }
+  cap->data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (cap->data == MAP_FAILED) {
+    close(fd);
+    cap->failed = 1;
+    return;
+  }
+  struct wl_shm_pool *pool = wl_shm_create_pool(cap->shm, fd, (int)size);
+  cap->buffer = wl_shm_pool_create_buffer(pool, 0, (int)width, (int)height,
+                                          (int)stride, WL_SHM_FORMAT_ARGB8888);
+  wl_shm_pool_destroy(pool);
+  close(fd);
+  zwlr_screencopy_frame_v1_copy(frame, cap->buffer);
+}
+
+static void frame_flags(void *data, struct zwlr_screencopy_frame_v1 *frame,
+                        uint32_t flags) {
+  (void)data;
+  (void)frame;
+  (void)flags;
+}
+
+static void frame_ready(void *data, struct zwlr_screencopy_frame_v1 *frame,
+                        uint32_t tv_sec_hi, uint32_t tv_sec_lo, uint32_t tv_nsec) {
+  (void)frame;
+  (void)tv_sec_hi;
+  (void)tv_sec_lo;
+  (void)tv_nsec;
+  struct capture *cap = data;
+  cap->done = 1;
+}
+
+static void frame_failed(void *data, struct zwlr_screencopy_frame_v1 *frame) {
+  (void)frame;
+  struct capture *cap = data;
+  cap->failed = 1;
+}
+
+static const struct zwlr_screencopy_frame_v1_listener frame_listener = {
+    .buffer = frame_buffer,
+    .flags = frame_flags,
+    .ready = frame_ready,
+    .failed = frame_failed,
+};
+
+static void cleanup_capture(struct capture *cap) {
+  if (cap->buffer) {
+    wl_buffer_destroy(cap->buffer);
+  }
+  if (cap->frame) {
+    zwlr_screencopy_frame_v1_destroy(cap->frame);
+  }
+  struct output *out, *tmp;
+  wl_list_for_each_safe(out, tmp, &cap->outputs, link) {
+    wl_output_destroy(out->wl_output);
+    free(out);
+  }
+  if (cap->shm) {
+    wl_shm_destroy(cap->shm);
+  }
+  if (cap->manager) {
+    zwlr_screencopy_manager_v1_destroy(cap->manager);
+  }
+  if (cap->registry) {
+    wl_registry_destroy(cap->registry);
+  }
+  if (cap->display) {
+    wl_display_disconnect(cap->display);
+  }
+}
+
+MMBitmapRef capture_screen_wayland(int32_t x, int32_t y, int32_t w, int32_t h,
+                                   int32_t display_id, int8_t isPid) {
+  (void)display_id;
+  (void)isPid;
+  struct capture cap = {0};
+  wl_list_init(&cap.outputs);
+
+  cap.display = wl_display_connect(NULL);
+  if (!cap.display) {
+    return NULL;
+  }
+  cap.registry = wl_display_get_registry(cap.display);
+  if (!cap.registry) {
+    cleanup_capture(&cap);
+    return NULL;
+  }
+  wl_registry_add_listener(cap.registry, &registry_listener, &cap);
+  wl_display_roundtrip(cap.display);
+
+  if (!cap.manager || !cap.shm || wl_list_empty(&cap.outputs)) {
+    cleanup_capture(&cap);
+    return NULL;
+  }
+  struct output *out = wl_container_of(cap.outputs.next, out, link);
+  cap.frame = zwlr_screencopy_manager_v1_capture_output(cap.manager, 0,
+                                                        out->wl_output);
+  zwlr_screencopy_frame_v1_add_listener(cap.frame, &frame_listener, &cap);
+
+  while (!cap.done && !cap.failed) {
+    wl_display_dispatch(cap.display);
+  }
+
+  if (cap.failed || !cap.data) {
+    cleanup_capture(&cap);
+    return NULL;
+  }
+
+  if (x < 0) x = 0;
+  if (y < 0) y = 0;
+  if (x > cap.width || y > cap.height) {
+    cleanup_capture(&cap);
+    return NULL;
+  }
+  if (w <= 0 || x + w > cap.width) {
+    w = cap.width - x;
+  }
+  if (h <= 0 || y + h > cap.height) {
+    h = cap.height - y;
+  }
+
+  size_t stride = (size_t)w * 4;
+  uint8_t *rgba = malloc(stride * (size_t)h);
+  if (!rgba) {
+    cleanup_capture(&cap);
+    return NULL;
+  }
+  uint8_t *src = cap.data;
+  for (int row = 0; row < h; ++row) {
+    for (int col = 0; col < w; ++col) {
+      size_t sidx = (size_t)(row + y) * cap.stride + (size_t)(col + x) * 4;
+      size_t didx = (size_t)row * stride + (size_t)col * 4;
+      rgba[didx + 0] = src[sidx + 2];
+      rgba[didx + 1] = src[sidx + 1];
+      rgba[didx + 2] = src[sidx + 0];
+      rgba[didx + 3] = src[sidx + 3];
+    }
+  }
+
+  munmap(cap.data, (size_t)cap.stride * (size_t)cap.height);
+  cleanup_capture(&cap);
+
+  MMBitmapRef bitmap = createMMBitmap_c(rgba, w, h, stride, 32, 4);
+  if (!bitmap) {
+    free(rgba);
+  }
+  return bitmap;
+}
+
+#endif  // IS_LINUX
